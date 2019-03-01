98a782175a56f801f6c4c4c7f2b075fb1a69fd71
canikwe <c.anikwe@gmail.com>
2019-03-01 07:31:38 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,141 +1,123 @@
 'use strict'
-document.addEventListener("DOMCintentLoaded", console.log("Ruff"), init())
+document.addEventListener("DOMCintentLoaded", init())
+
+//set filterBtn to false for good dog filter button
+let filter = false
 
 function init() {
+  console.log("Ruff")
   //declare variable for event listener
   let filterBtn = document.querySelector("#good-dog-filter")
   
-  //render all pups on page load
+  //grab all pups from the db
   getAllPups()
   
   //add event listener to filterBtn and set active dataset for filter tracking
-  filterBtn.dataset.active = false
-  filterBtn.addEventListener("click", filterPups)
+  filterBtn.addEventListener("click", () => {filterPups(filterBtn)})
 }
-
 
 function getAllPups() {
+  //set varable for dog bar (div) to hold all the pups names
+  const dogBar = document.querySelector("#dog-bar")
+
   //initial fetch for all pups
   fetch('http://localhost:3000/pups')
-	 .then(res => res.json())
-	 .then(json => {
-    json.forEach((el) => renderPup(el))
+  .then(res => res.json())
+  .then(pups => {
+    dogBar.innerText = '' //clear dog bar to render new pups
+    pups.forEach((pup) => { //render only good pups if filterBtn is set to true. Render all if filterBtn is set to false
+      if (filter === true && pup.isGoodDog === true) {
+        renderPup(pup, dogBar)
+      } else if (filter === false) {
+        renderPup(pup, dogBar)
+      }
     })
+  })
 }
 
-function renderPup(dog) {
+function renderPup(dog, dogBar) {
   //create inidivual dog span and set span attributes with dog info
-  let dogBar = document.querySelector("#dog-bar")
-  let span = document.createElement("span")
+  const span = document.createElement("span")
   
-  span.dataset.dogId = dog.id
-  span.dataset.goodBool = dog.isGoodDog
+  //set span attributes and add event listener
   span.innerText = dog.name
-  span.className = `dogs dog-${dog.id}`
-  span.addEventListener('click', fetchDog)
+  span.addEventListener('click', () => {displayDog(dog)})
   
-  //append dog span to dogbar
+  //append span to dogBar
   dogBar.appendChild(span)
 }
 
-function fetchDog(e) {
-  //individual dog fetch for pup picture
-  let id = e.currentTarget.dataset.dogId
-  fetch(`http://localhost:3000/pups/${id}`)
-    .then(res => res.json())
-    .then(json => displayDog(json))
-}
-
 function displayDog(dog){
-  //create elements for pup rendering
-  let dogInfo = document.querySelector("#dog-info")
-  let h2 = document.createElement("h2")
-  let img = document.createElement("img")
-  let btn = document.createElement("button")
+  //query and create elements for pup rendering
+  const dogInfo = document.querySelector("#dog-info")
+  const h2 = document.createElement("h2")
+  const img = document.createElement("img")
+  const btn = document.createElement("button")
   
-  //clear dogInfo div of previous dog's info and set new pup attributes to elements for rendering and event histener handling
+  //clear dogInfo div of previous dog's info and set new pup attributes to elements for rendering and event listener handling
   dogInfo.innerHTML = ''
   h2.innerText = dog.name
   img.src = dog.image
-  btn.dataset.isGoodDog = dog.isGoodDog
-  btn.dataset.id = dog.id
-  //  debugger
-  btn.innerText = goodDogBtn(btn)
-  btn.addEventListener('click', goodBoiDbUpdate)
+  btn.innerText = goodDogBtn(dog)
   
-  //add updated pup info to dog-info div
+  //add event listener to btn to update isGoodDog status
+  btn.addEventListener('click', () => {
+    goodBoiDOMUpdate(btn, dog)
+  })
+  
+  //append elements to dogInfo div
   dogInfo.appendChild(img)
   dogInfo.appendChild(h2)
   dogInfo.appendChild(btn)
     
 }
 
-function goodDogBtn(btn) {
-  //render goodDog button with text cooresponding to isGoodDog status
-  let btnBool = JSON.parse(btn.dataset.isGoodDog)
-  
-  return btnBool ? "Good Dog!" :  "Bad Dog!"
+function goodDogBtn(dog) {
+  //render button with text cooresponding to isGoodDog status
+  return dog.isGoodDog ? "Good Dog!" :  "Bad Dog!"
 }
 
-function goodBoiDbUpdate(e) {
-  e.preventDefault()
-  //update goodDog status in the database
-  let id = e.currentTarget.dataset.id
-  let data = JSON.parse(e.currentTarget.dataset.isGoodDog)
+function goodBoiDOMUpdate(btn, dog){
+  //update isGoodDog boolean
+  dog.isGoodDog = !dog.isGoodDog
   
-  goodBoiDOMUpdate(e)
+  //update button innerText
+  btn.innerText = goodDogBtn(dog)
   
-  fetch(`http://localhost:3000/pups/${id}`, {
+  //send dog for db PATCH update
+  goodBoiDbUpdate(dog)
+}
+
+function goodBoiDbUpdate(dog) {
+  //data being sent to db for patch
+  const data = {isGoodDog: dog.isGoodDog}
+  
+  fetch(`http://localhost:3000/pups/${dog.id}`, {
     method: "PATCH",
     headers: 
     {
       "Content-Type": "application/json",
       Accept: "application/json"
     },
-    body: JSON.stringify({isGoodDog: data})
+    body: JSON.stringify(data) //must stringify to send info to db
     })
   .then(res => res.json())
   .then(json => console.log(json))
 }
 
-function goodBoiDOMUpdate(e){
-  //update attributes in dog bar and dog info divs with new goodBoi status
-  let btn = e.currentTarget
-  let btnBool = JSON.parse(btn.dataset.isGoodDog)
-  let dogBarId = document.querySelector(`.dog-${btn.dataset.id}`)
-
-  dogBarId.dataset.goodBool = !btnBool
-  btn.dataset.isGoodDog = !btnBool
-
-  btn.innerText = goodDogBtn(btn)
-}
-
-function filterPups(e) {
-  e.preventDefault()
-  let dogInfo = document.querySelector("#dog-info")
-  let filterBool = !(JSON.parse(e.target.dataset.active))
-  e.target.dataset.active = filterBool
+function filterPups(filterBtn) {
+  //update filter variable on click
+  filter = !filter
   
-  if (filterBool) {
-    document.querySelector("#good-dog-filter").innerText = "Filter good dogs: ON"
-    document.querySelectorAll(".dogs").forEach((dog) => {
-      let goodBool = JSON.parse(dog.dataset.goodBool)
-      
-      if (goodBool) {
-    //render good dogs only when filterBool === true 
-        dog.style.display = "flex"
-        dogInfo.innerText = ''
-        } else {
-    //toggle element display off when isGoodBoy === false
-          dog.style.display = "none"
-        }
-    })
+  //update filter button innertext on click
+  if (filter) {
+    filterBtn.innerText = "Filter good dogs: ON"
   } else {
-    //render all dogs when filterBool === false
-      document.querySelector("#good-dog-filter").innerText = "Filter good dogs: OFF"
-      document.querySelectorAll(".dogs").forEach((dog) => { dog.style.display = "flex" })
-      dogInfo.innerText = ''
+    filterBtn.innerText = "Filter good dogs: OFF"
   }
-
+  
+  //clear dogInfo div and render all pups to the dogBar
+  document.querySelector("#dog-info").innerText = ''
+  getAllPups()
 }
 
